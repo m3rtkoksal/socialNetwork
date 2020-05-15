de9947af03fda4902bdbeb2d38a0d03c39e00a4f
MK <mertkoksal88@gmail.com>
2020-05-15 13:54:15 +0300

    Initial Commit

---
+++ socialNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  socialNetwork
+//
+//  Created by Mert Köksal on 15.05.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
